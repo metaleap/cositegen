0b005ffd27a30dea43a7b02b9e7c4370e377e439
Phil Schumann <metaleap@users.noreply.github.com>
2022-08-20 12:41:35 +0200

    [autopush] «Microwaves frizz your heir.»

--- go-tview-reflectreeview/reflectreeview.go
+++ go-tview-reflectreeview/reflectreeview.go
@@ -6,7 +6,7 @@ import (
 )
 
 var MainQuitOnKeyEvents = [][3]int{
-	{int(tcell.ModCtrl), 0, 'q'}, // ctrl+q
+	{int(tcell.ModCtrl | tcell.ModAlt), 0, 'q'}, // ctrl+q
 	// {0, int(tcell.KeyESC), 0},    // esc
 }
 
@@ -14,16 +14,17 @@ func Main(enableMouse bool, data any, showRoot bool) error {
 	app := tview.NewApplication()
 	app.EnableMouse(enableMouse)
 	tree := New(data, showRoot)
-	app.SetInputCapture(func(key *tcell.EventKey) *tcell.EventKey {
-		for _, quitter := range MainQuitOnKeyEvents {
-			if (quitter[0] < 0 || key.Modifiers() == tcell.ModMask(quitter[0])) &&
-				key.Key() == tcell.Key(quitter[1]) && key.Rune() == rune(quitter[2]) {
-				app.Stop()
-				return nil
-			}
-		}
-		return key
-	})
+	// app.SetInputCapture(func(key *tcell.EventKey) *tcell.EventKey {
+	// 	for _, quitter := range MainQuitOnKeyEvents {
+	// 		if (quitter[0] < 0 || key.Modifiers() == tcell.ModMask(quitter[0])) &&
+	// 			((quitter[1] > 0 && key.Key() == tcell.Key(quitter[1])) || (quitter[2] > 0 && key.Rune() == rune(quitter[2]))) {
+	// 			panic("YOY")
+	// 			app.Stop()
+	// 			return nil
+	// 		}
+	// 	}
+	// 	return key
+	// })
 	return app.SetRoot(tree, true).Run()
 }
 
@@ -32,11 +33,13 @@ func New(data any, showRoot bool) *tview.TreeView {
 	if !showRoot {
 		tree.SetTopLevel(1)
 	}
-	tree.SetRoot(newNode(data))
+	node := newNode(data)
+	tree.SetRoot(node).SetCurrentNode(node)
 	return tree
 }
 
 func newNode(data any) *tview.TreeNode {
 	node := tview.NewTreeNode("foo")
+
 	return node
 }
